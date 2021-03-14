8a5467edae396866f5749e8c1e78b41803789ab6
MichailShcherbakov <50011226+MichailShcherbakov@users.noreply.github.com>
2021-03-14 17:43:04 +0300

    Update build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,23 @@
+import common from './common';
 import srp from './srp';
-export { srp };
+export { common, srp };
+declare const _default: {
+    common: {
+        genAccountId: ({ versionCode, userId }: import("./common/genAccountId").Options) => string;
+        genCryptoRandomString: (length: number) => string;
+        genSalt: () => string;
+    };
+    srp: {
+        client: {
+            deriveVerifier: (secretKey: string, password: string) => import("./srp/client/deriveVerifier").Verifier;
+            deriveSession: (salt: string, secretKey: string, password: string, clientSecretEphemeral: string, serverPublicEphemeral: string) => import("secure-remote-password/client").Session;
+            verifySession: (clientPublicEphemeral: string, clientSession: import("secure-remote-password/client").Session, serverSessionProof: string) => void;
+            generateEphemeral: () => import("secure-remote-password/client").Ephemeral;
+        };
+        server: {
+            deriveSession: (serverSecretEphemeral: string, clientPublicEphemeral: string, salt: string, secretKey: string, verifier: string, clientSessionProof: string) => import("secure-remote-password/server").Session;
+            generateEphemeral: (verifier: string) => import("secure-remote-password/server").Ephemeral;
+        };
+    };
+};
+export default _default;
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.srp = void 0;
+exports.srp = exports.common = void 0;
+const common_1 = require("./common");
+exports.common = common_1.default;
 const srp_1 = require("./srp");
 exports.srp = srp_1.default;
+exports.default = { common: common_1.default, srp: srp_1.default };
 //# sourceMappingURL=index.js.map
